e5ceb9a08a4953930a5d0a011f408bb916530ba5
ertrii <ertriilive@gmail.com>
2018-09-04 20:21:15 -0500

    init

--- Moonered-client/Program.cs
+++ Moonered-client/Program.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net;
-using System.Net.Sockets;
 
-namespace Moonered
+namespace Moonered_client
 {
     class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
